fc1b936dc882c95325d733e12fd8d3df4ddc98d0
Will Corcoran <corcoranwill@gmail.com>
2020-02-13 00:58:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-def name_printer (name)
+def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
-def name_language_printer (name, language)
+def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
